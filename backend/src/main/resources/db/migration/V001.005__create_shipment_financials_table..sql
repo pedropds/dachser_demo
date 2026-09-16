fcc8c87c864ba847318ed465ddf2559f503c0d2b
Pedro Sousa <pedropintodsousa@gmail.com>
2026-09-16 13:30:17 +0100

    Small change to shipment financial table. Added foreign keys for both cost and income tables, for easy of audit.

--- backend/src/main/resources/db/migration/V001.005__create_shipment_financials_table..sql
+++ backend/src/main/resources/db/migration/V001.005__create_shipment_financials_table..sql
@@ -1,14 +1,19 @@
 -- This table stores individual profit/loss calculation records.
--- Each row represents the outcome of a specific calculation event
--- (a distinct set of income and costs processed at one time),
--- rather than the grand running total for the entire shipment.
+-- Each row represents the outcome of a specific calculation event.
 CREATE TABLE shipment_financials
 (
     id             BIGINT AUTO_INCREMENT PRIMARY KEY,
     shipment_id    BIGINT         NOT NULL,
+
+    -- Audit Links
+    income_ids     BIGINT ARRAY,
+    cost_ids       BIGINT ARRAY,
+
+    -- Calculated Values
     income         DECIMAL(10, 2) NOT NULL,
     total_costs    DECIMAL(10, 2) NOT NULL,
     profit_or_loss DECIMAL(10, 2) NOT NULL,
     calculated_at  TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+
     FOREIGN KEY (shipment_id) REFERENCES shipments (id)
 );
