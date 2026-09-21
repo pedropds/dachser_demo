ae94211eb98b574ba4e25a42cd40c89523f0487a
Pedro Sousa <pedropintodsousa@gmail.com>
2026-09-21 11:19:47 +0100

    Unsafe fix for CORS to work with insomnia as well

--- backend/src/main/java/com/dachser/demo/config/CorsConfig.java
+++ backend/src/main/java/com/dachser/demo/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(@NonNull CorsRegistry registry) {
                 registry.addMapping("/api/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
